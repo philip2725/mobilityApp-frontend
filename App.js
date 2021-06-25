672839bb38b3f88e2538b5e5eb76297c65544d21
philip2725 <haertel.philip12@gmail.com>
2021-06-25 12:28:56 +0200

    setting up directory structure and redux

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store} >
+      <View style={styles.container}>
+        <Text>Test1234</Text>
+
+      </View>
+    </Provider>
   );
 }
 
---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+import * as Colors from './colors';
+import * as Spacing from './spacing';
+import * as Typography from './typography';
+
+export { Typography, Spacing, Colors };
+
+
