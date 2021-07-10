40232bbadbf2ccdde04d909a23bcc418ad585d37
philip2725 <haertel.philip12@gmail.com>
2021-07-10 10:30:21 +0200

    added firebase to project

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import RootNav from './src/navigation';
+import firebase from 'firebase';
+import {firebaseConfig} from './src/config/firebase';
+
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
 
---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyBPxwE6w62hOtR-ECeNCBCn-WeFS-IHSuk",
+    authDomain: "bachelor-thesis-99898.firebaseapp.com",
+    projectId: "bachelor-thesis-99898",
+    storageBucket: "bachelor-thesis-99898.appspot.com",
+    messagingSenderId: "679724362165",
+    appId: "1:679724362165:web:20ca4632530b262a9451fc",
+    measurementId: "G-MNYWSCDZD1"
+  };
